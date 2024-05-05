3520f6b18a36d6db4ff6bcb4eb6b7f5eb4367677
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-05-06 01:55:30 +0900

    전역 인트로 페이지 추가

---
+++ frontend/src/store/useIntroState.tsx
@@ -0,0 +1,12 @@
+// import { create } from 'zustand'
+
+// interface IntroAnimationState {
+//   loadingState: boolean;
+//   dimensionState: 'loadiong' | 'complete'
+// }
+
+// const useIntroAnimationState = create<IntroAnimationState>((set) => {
+//   loadingState: true;
+//   updateState: () => set((state) => !state)
+
+// })
